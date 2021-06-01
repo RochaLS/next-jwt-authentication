22cdd71f92626821ca209e39bcc0b14192b96b75
Lucas Rocha <ld.rocha@hotmail.com>
2021-06-01 13:43:21 -0700

    create axios request queue to handle failed requests

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -58,7 +58,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         path: '/'
       })
 
-      setCookie(undefined, 'nextauth.refreshToken', token, {
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/'
       })
